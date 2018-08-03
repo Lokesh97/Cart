f1f63cbd8c38eabf4b7c96d58f6b865ed7883d17
HimanshuGarg786 <Himanshugarg705@gmail.com>
2018-08-03 16:58:48 +0530

    product schema added

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,4 +1,51 @@
+
+
 public class Merchant {
-    private int mid;
-    private int count;
+
+    private int merchantId;
+
+    private String merchantName;
+
+    private double merchantRating;
+
+    private String merchantContact;
+
+    public Merchant(int merchantId, String merchantName, double merchantRating, String merchantContact) {
+        this.merchantId = merchantId;
+        this.merchantName = merchantName;
+        this.merchantRating = merchantRating;
+        this.merchantContact = merchantContact;
+    }
+
+    public int getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(int merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public String getMerchantName() {
+        return merchantName;
+    }
+
+    public void setMerchantName(String merchantName) {
+        this.merchantName = merchantName;
+    }
+
+    public double getMerchantRating() {
+        return merchantRating;
+    }
+
+    public void setMerchantRating(double merchantRating) {
+        this.merchantRating = merchantRating;
+    }
+
+    public String getMerchantContact() {
+        return merchantContact;
+    }
+
+    public void setMerchantContact(String merchantContact) {
+        this.merchantContact = merchantContact;
+    }
 }
